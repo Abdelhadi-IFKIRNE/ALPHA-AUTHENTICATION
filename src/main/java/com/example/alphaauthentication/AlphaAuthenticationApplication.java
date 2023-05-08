b4ec343fabfb57ab4f431c2519f964f148c44536
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-08 17:01:10 +0000

    update ...

---
+++ src/main/java/com/example/alphaauthentication/AlphaAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphaauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alphaauthentication/AlphaAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphaauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
